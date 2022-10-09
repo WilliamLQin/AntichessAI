1b05813f96e32c4a73d38dc354977713c664f1ce
William Qin <william.qin51@gmail.com>
2022-10-09 00:55:07 -0400

    Import cpp chess, fix bugs https://github.com/pjpuzzler/cpp-chess

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include "Board/Board.h"
+#include "cpp-chess/chess.h"
+
+const int MAX_DEPTH = 6;
+
+chess::Board board;
+
+void traverse(int depth) {
+    if (depth >= MAX_DEPTH) {
+        return;
+    }
+    std::vector<chess::Move> moves = board.legal_moves();
+    for (auto move : moves) {
+        board.push(move);
+        traverse(depth+1);
+        board.pop();
+    }
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
     Board b;
     b.hello();
+
+    traverse(0);
 
     return 0;
 }
