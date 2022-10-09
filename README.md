6f6e4e61ae63b7430100232b22113ce5d29de7d2
William Qin <william.qin51@gmail.com>
2022-10-09 15:15:56 -0400

    allow pruning prints

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 AI to play forced capture chess, but we still want to win
 
 Created for CO 456 project
+
+# References
+
+src/cpp-chess sourced from https://github.com/pjpuzzler/cpp-chess
--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,24 @@
 #include <stdlib.h>
 #include "cpp-chess/chess.h"
 
-//const int MAX_DEPTH = 3;
-//
-//void traverse(int depth, chess::Board &board) {
-//    std::cout << "DEPTH: " << depth << std::endl;
-//    std::cout << std::string(board) << std::endl;
-//    if (depth >= MAX_DEPTH) {
-//        return;
-//    }
-//    std::vector<chess::Move> moves = board.legal_moves();
-//    for (auto move : moves) {
-//        board.push(move);
-//        traverse(depth+1, board);
-//        board.pop();
-//    }
-//}
+const int MAX_DEPTH = 6;
+const bool PRINT_INFO = false;
 
-int main() {
-    srand(getpid());
+void traverse(int depth, chess::Board &board) {
+    std::cout << "DEPTH: " << depth << std::endl;
+    std::cout << std::string(board) << std::endl;
+    if (depth >= MAX_DEPTH) {
+        return;
+    }
+    std::vector<chess::Move> moves = board.legal_moves();
+    for (auto move : moves) {
+        board.push(move);
+        traverse(depth+1, board);
+        board.pop();
+    }
+}
 
-    std::cout << "Welcome to Antichess AI for CO 456" << std::endl;
-
+void play_game() {
     chess::Board::captures_compulsory = true;
     chess::Board board("1r3r2/7p/2k5/8/3K3p/8/p7/8 w - - 0 1");
 
@@ -39,7 +36,14 @@ int main() {
         int moveIndex = rand() % moves.size();
         board.push(moves[moveIndex]);
 
-        std::cout << std::endl << std::string(board) << std::endl << std::endl;
+        if (PRINT_INFO) {
+            std::cout << "Computer plays: " << std::flush;
+        }
+        std::cout << std::string(moves[moveIndex]) << std::endl;
+
+        if (PRINT_INFO) {
+            std::cout << std::endl << std::string(board) << std::endl << std::endl;
+        }
 
         if (board.is_game_over()) {
             break;
@@ -51,15 +55,19 @@ int main() {
             moves = board.generate_legal_moves();
         }
 
-        std::cout << "Valid moves:";
-        for (auto move : moves) {
-            std::cout << " " << move.uci();
+        if (PRINT_INFO) {
+            std::cout << "Valid moves:";
+            for (auto move: moves) {
+                std::cout << " " << move.uci();
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
 
         bool found = false;
         while(!found) {
-            std::cout << "Your move: " << std::flush;
+            if (PRINT_INFO) {
+                std::cout << "Your move: " << std::flush;
+            }
             std::cin >> input;
 
             try {
@@ -73,7 +81,7 @@ int main() {
                 }
             } catch (...) {}
 
-            if (!found) {
+            if (!found && PRINT_INFO) {
                 std::cout << "Invalid move." << std::endl;
             }
         }
@@ -83,8 +91,20 @@ int main() {
         }
     }
 
-    std::cout << std::endl << "Game over!" << std::endl;
-    std::cout << std::string(board) << std::endl;
+    if (PRINT_INFO) {
+        std::cout << std::endl << "Game over!" << std::endl;
+        std::cout << std::string(board) << std::endl;
+    }
+}
+
+int main() {
+    srand(getpid());
+
+//    std::cout << "Welcome to Antichess AI for CO 456" << std::endl;
+//    chess::Board board;
+//    traverse(0, board);
+
+    play_game();
 
     return 0;
 }
