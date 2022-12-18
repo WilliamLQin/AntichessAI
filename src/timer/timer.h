31bfd3f7b9e2f5ffbed67c442b429c5a761a7e86
William Qin <william.qin51@gmail.com>
2022-12-18 18:36:40 -0500

    triple amount of time to think

--- src/timer/timer.h
+++ src/timer/timer.h
@@ -11,7 +11,7 @@
 class Timer {
     constexpr static std::chrono::milliseconds ALLOTTED_TIME = std::chrono::milliseconds(3*60*1000);
     constexpr static std::chrono::milliseconds LEEWAY_TIME = std::chrono::milliseconds(100);
-    constexpr static std::chrono::milliseconds TIME_PER_TURN = std::chrono::milliseconds (1000);
+    constexpr static std::chrono::milliseconds TIME_PER_TURN = std::chrono::milliseconds (3000);
     std::chrono::milliseconds usedTime;
     std::chrono::steady_clock::time_point startTime;
 public:
